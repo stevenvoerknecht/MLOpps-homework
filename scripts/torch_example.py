a49d67f4a2266db3768a8e1843248693b9216780
EliaLoeb <elia.loeb@student.uva.nl>
2026-01-20 11:46:46 +0100

    8.3 job runs

--- scripts/torch_example.py
+++ scripts/torch_example.py
@@ -1,40 +1,26 @@
 import torch
 import time
-
-model = torch.nn.Sequential(
-    torch.nn.Linear(2, 1024),
-    torch.nn.ReLU(),
-    torch.nn.Dropout(p=0.5),
-    torch.nn.Linear(1024, 1),
-    torch.nn.Sigmoid()
-)
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
 
+model = torch.nn.Sequential(
+    torch.nn.Linear(4096, 4096),
+    torch.nn.ReLU(),
+    torch.nn.Linear(4096, 4096),
+    torch.nn.ReLU(),
+    torch.nn.Linear(4096, 4096),
+    torch.nn.Linear(4096, 1)
+).to(device)
 
-dummy_input = torch.randn(1, 2).to(device)
+batch_size = 128
+dummy_input = torch.randn(batch_size, 4096).to(device)
 
-print(f"Starting benchmark on {device}...")
-
-for _ in range(10):
-    _ = model(dummy_input)
-if device.type == 'cuda': torch.cuda.synchronize()
-
-start_time = time.time()
-iterations = 1000  # We doen er wat meer voor een betere meting op GPU
-for _ in range(iterations):
-    _ = model(dummy_input)
-    
-if device.type == 'cuda': torch.cuda.synchronize()
-
-end_time = time.time()
-
-total_time = end_time - start_time
-throughput = iterations / total_time
-
-print(f"Total time for {iterations} iterations: {total_time:.4f} seconds")
-print(f"Throughput: {throughput:.2f} img/s")
+output = model(dummy_input)
 
 if device.type == 'cuda':
-    print(f"GPU Name: {torch.cuda.get_device_name(0)}")
+    max_memory = torch.cuda.max_memory_allocated(device) / (1024 ** 2)
+    print(f"GPU Naam: {torch.cuda.get_device_name(0)}")
+    print(f"Max VRAM: {max_memory:.2f} MB")
+else:
+    print("Geen GPU gevonden")
